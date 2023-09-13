5af61cf2eadf748a52d4c4138d1741ac45e17301
Antonio Apolinario Barbosa <aab2@WINDOWS.CIN.UFPE.BR>
2023-09-13 15:44:30 -0300

    .

--- Quartus/pcihello.sld
+++ Quartus/pcihello.sld
@@ -2,8 +2,8 @@
   <sld_infos>
     <sld_info hpath="pcihellocore:u0" name="u0">
       <assignment_values>
-        <assignment_value text="QSYS_NAME pcihellocore HAS_SOPCINFO 1 GENERATION_ID 1693968392"/>
-        <assignment_value text="QSYS_NAME pcihellocore HAS_SOPCINFO 1 GENERATION_ID 1693969342"/>
+        <assignment_value text="QSYS_NAME pcihellocore HAS_SOPCINFO 1 GENERATION_ID 1694629465"/>
+        <assignment_value text="QSYS_NAME pcihellocore HAS_SOPCINFO 1 GENERATION_ID 1694629522"/>
       </assignment_values>
     </sld_info>
   </sld_infos>
--- Quartus/pcihellocore/pcihellocore_bb.v
+++ Quartus/pcihellocore/pcihellocore_bb.v
@@ -1,34 +1,34 @@
 
 module pcihellocore (
-	pcie_hard_ip_0_rx_in_rx_datain_0,
-	pcie_hard_ip_0_tx_out_tx_dataout_0,
-	pcie_hard_ip_0_powerdown_pll_powerdown,
-	pcie_hard_ip_0_powerdown_gxb_powerdown,
-	pcie_hard_ip_0_refclk_export,
-	pcie_hard_ip_0_pcie_rstn_export,
-	inport_external_connection_export,
-	hex_display_2_external_connection_export,
-	hex_display_external_connection_export,
-	switches_external_connection_export,
 	fan_control_external_connection_in_port,
 	fan_control_external_connection_out_port,
 	green_leds_external_connection_export,
+	hex_display_2_external_connection_export,
+	hex_display_external_connection_export,
+	inport_external_connection_export,
+	pcie_hard_ip_0_pcie_rstn_export,
+	pcie_hard_ip_0_powerdown_pll_powerdown,
+	pcie_hard_ip_0_powerdown_gxb_powerdown,
+	pcie_hard_ip_0_refclk_export,
+	pcie_hard_ip_0_rx_in_rx_datain_0,
+	pcie_hard_ip_0_tx_out_tx_dataout_0,
+	push_buttons_external_connection_export,
 	red_leds_external_connection_export,
-	push_buttons_external_connection_export);	
+	switches_external_connection_export);	
 
-	input		pcie_hard_ip_0_rx_in_rx_datain_0;
-	output		pcie_hard_ip_0_tx_out_tx_dataout_0;
-	input		pcie_hard_ip_0_powerdown_pll_powerdown;
-	input		pcie_hard_ip_0_powerdown_gxb_powerdown;
-	input		pcie_hard_ip_0_refclk_export;
-	input		pcie_hard_ip_0_pcie_rstn_export;
-	input	[31:0]	inport_external_connection_export;
-	output	[31:0]	hex_display_2_external_connection_export;
-	output	[31:0]	hex_display_external_connection_export;
-	input	[31:0]	switches_external_connection_export;
 	input		fan_control_external_connection_in_port;
 	output		fan_control_external_connection_out_port;
 	output	[31:0]	green_leds_external_connection_export;
-	output	[31:0]	red_leds_external_connection_export;
+	output	[31:0]	hex_display_2_external_connection_export;
+	output	[31:0]	hex_display_external_connection_export;
+	input	[31:0]	inport_external_connection_export;
+	input		pcie_hard_ip_0_pcie_rstn_export;
+	input		pcie_hard_ip_0_powerdown_pll_powerdown;
+	input		pcie_hard_ip_0_powerdown_gxb_powerdown;
+	input		pcie_hard_ip_0_refclk_export;
+	input		pcie_hard_ip_0_rx_in_rx_datain_0;
+	output		pcie_hard_ip_0_tx_out_tx_dataout_0;
 	input	[31:0]	push_buttons_external_connection_export;
+	output	[31:0]	red_leds_external_connection_export;
+	input	[31:0]	switches_external_connection_export;
 endmodule
