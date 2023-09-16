14221c0275c939611afd0e10282cf1c266773eff
Ariovaldo Neto <a.neto@strategiesnorth.ca>
2023-09-15 23:46:49 -0300

    working betterer?

--- app/main.py
+++ app/main.py
@@ -18,70 +18,63 @@ def main():
         print("Syntax: %s </dev/device_file>"%sys.argv[0])
         exit(1)
 
-    global fd
     fd = os.open(sys.argv[1], os.O_RDWR)
-
-    #wait until fd is loaded
-    print("loading")
-    sleep(3)
-    print("loaded\n\n")
-
-    # data to write
-    data = 0x40404079;
-
-    #liga_led(0b101010101010101010, WR_RED_LEDS)
-    #liga_led(0b101010101, WR_GREEN_LEDS)
+    cubo = 0b0100_0110_0100_0001_0000_0011_0100_0000
     
-    #sleep(2)
-    for i in range(2):
-        #liga_led(0b1001111010011110, WR_R_DISPLAY)
-        liga_led(0b010001100100000010000001101000000, WR_R_DISPLAY)
+    try:
+        #wait until fd is loaded
+        print("loading")
+        sleep(3)
+        print("loaded\n\n")
 
-    
-#    with os.fdopen(os.open(sys.argv[1], os.O_RDWR), "w") as fdd:
-#        ioctl(fdd, RD_SWITCHES)
-#        k = os.read(fdd, 4)
-#        n = int.from_bytes(red, 'little')
-#        print(f"{n:016b}")
-    #for i in range(0, 5):
-        #data = i
+        # data to write
+        data = 0x40404079;
 
-        #sw = le_switch()
-        #b = le_botao()
-        #liga_led(i, WR_R_DISPLAY)
-        #liga_led(sw, WR_RED_LEDS)
-        #sleep(1)
-        #liga_led(b, WR_GREEN_LEDS)
-        #sleep(1)
-	#liga_led(b, WR_L_DISPLAY)
-        #liga_led(b, WR_R_DISPLAY)
-        #sleep(0.005) 
+        #liga_led(0b101010101010101010, WR_RED_LEDS)
+        #liga_led(0b101010101, WR_GREEN_LEDS)
+        # primeiro 0 é o decimal
+        #sleep(2)
+        for i in range(30):
+            liga_led(fd, bitwise_left_shift_wraparound(cubo, 8*i, 32), WR_L_DISPLAY)
+            liga_led(fd, bitwise_left_shift_wraparound(cubo, 8*i, 32), WR_L_DISPLAY)
+            liga_led(fd, bitwise_left_shift_wraparound(cubo, 8*i, 32), WR_R_DISPLAY)
+            liga_led(fd, bitwise_left_shift_wraparound(cubo, 8*i, 32), WR_R_DISPLAY)
+            sleep(0.5)
 
-    print("botao")
+        sleep(2)
 
-    while True:
-        b = le_botao()
-        sw = le_switch()
-        liga_led(b, WR_GREEN_LEDS)
-        liga_led(sw, WR_RED_LEDS)
-        liga_led(sw, WR_R_DISPLAY)
-        liga_led(sw, WR_L_DISPLAY)
-        if b == 0b0101:
-            break
-    
-    # for i in range(20):
-    #     ioctl(fd, RD_PBUTTONS)
-    #     red = os.read(fd, 4); # read 4 bytes and store in red var
-    #     red = os.read(fd, 4); # read 4 bytes and store in red var
-    #     n = int.from_bytes(red, 'little')
-    #     print(f"button {n:04b}")
-    #     sleep(0.5)
+        print("botao")
 
-    
+        while True:
+            b = le_botao(fd)
+            sw = le_switch(fd)
+            liga_led(fd, b, WR_GREEN_LEDS)
+            liga_led(fd, sw, WR_RED_LEDS)
+            # liga_led(sw, WR_R_DISPLAY)
+            # liga_led(sw, WR_L_DISPLAY)
+            if b == 0b0101:
+                break
+
+    except OSError as e:
+        print(f"Error opening or accessing {fd}: {e}")
+        sys.exit(1)
 
     os.close(fd)
+    sys.exit(0)
 
-def le_switch():
+def bitwise_left_shift_wraparound(number, shift, bits):
+    # Ensure that shift is within the range of bits
+    shift %= bits
+    
+    # Calculate the part of the shift that wraps around
+    wraparound = (number >> (bits - shift)) & ((1 << shift) - 1)
+    
+    # Perform the remaining shift without wrap-around
+    result = ((number << shift) | wraparound) & ((1 << bits) - 1)
+    
+    return result
+
+def le_switch(fd):
     sleep(0.1)
     ioctl(fd, RD_SWITCHES)
     red = os.read(fd, 4); # read 4 bytes and store in red var
@@ -90,7 +83,7 @@ def le_switch():
     print(f"switch {n:016b}")
     return n
 
-def le_botao():
+def le_botao(fd):
     sleep(0.1)
     ioctl(fd, RD_PBUTTONS)
     red = os.read(fd, 4); # read 4 bytes and store in red var
@@ -99,11 +92,10 @@ def le_botao():
     print(f"buttons {n:04b}")
     return n
 
-def liga_led(leds, cor):
+def liga_led(fd, leds, cor):
     if (cor not in [WR_RED_LEDS, WR_GREEN_LEDS, WR_L_DISPLAY, WR_R_DISPLAY]):
         print ("nao existe essa cor")
         return 0
-    global fd
     ioctl(fd, cor)
     retval = os.write(fd, leds.to_bytes(4, 'little'))
     print(f"led ({cor}) [{retval}]: ({leds:018b})")
